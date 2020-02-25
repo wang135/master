57e9499eb1a8012a7965894888e1683ca409fce8
jingwang28 <ragvyj-nyvfej-6Kermi>
2020-02-25 16:22:10 +0800

    新增模型的代码9,是测试的1

---
+++ fin_renhang_riskcontrol-shengchan/Gonghangguize/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for Gonghangguize project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Gonghangguize.settings")
+
+#新修改的
+profile = os.environ.get('TYPEIDEA_PROFILE',)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Gonghangguize.settings.%s' % profile)
+
+application = get_wsgi_application()
